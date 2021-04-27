942bd17e8b04603d3c12c1c3aed1b93075cf2664
flavianski <githinjiflavian@gmail.com>
2021-04-27 04:10:29 +0300

    xray and Appointment

--- src/controller/RadiologyAppointmentStatus.java
+++ src/controller/RadiologyAppointmentStatus.java
@@ -1,6 +1,11 @@
 package controller;
 
-import models.Appointment;
+import models.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * The RadiologyAppointmentStatus class enables a clinician to create an appointment and follow up the appointment to see
@@ -15,7 +20,27 @@ public class RadiologyAppointmentStatus {
      * @return 1 is appointment is created successfully else 0
      */
     public static int placeAppointment(Appointment appointment){
+        DbConnection dbConnection = new DbConnection();
+        try {
+            Connection con = dbConnection.connectDb();
 
+            String query = "insert into appointment (id, status, title, patient_reg_no, clinician_staff_no) values(?, ?, ?, ?, ?);";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, appointment.getId());
+            preparedStatement.setString(2, appointment.getStatus());
+            preparedStatement.setString(3, appointment.getTitle());
+            preparedStatement.setInt(4, appointment.getPatient().getReq_no());
+            preparedStatement.setInt(5, appointment.getClinician().getStaff_no());
+
+
+
+
+            preparedStatement.execute();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return 1;
     }
 
@@ -47,12 +72,81 @@ public class RadiologyAppointmentStatus {
      * @return
      */
     public static int deleteAppointment(Appointment appointment){
+        DbConnection dbConnection = new DbConnection();
+        try {
+            Connection con = dbConnection.connectDb();
 
+            String query = "delete from appointment where id=?;";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, appointment.getId());
+
+
+            preparedStatement.execute();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return 1;
     }
 
-    public static  int updateAppointment(Appointment appointment){
+    public static  int updateAppointment(Appointment appointment, Patient patient, Clinician clinician){
+        DbConnection dbConnection = new DbConnection();
+        try {
+            Connection con = dbConnection.connectDb();
 
+            String query = "update appointment set  status=?, title=?, patient_reg_no=?, clinician_staff_no=? where id=?;";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setString(1, appointment.getStatus());
+            preparedStatement.setString(2, appointment.getTitle());
+            preparedStatement.setInt(3, patient.getReq_no());
+            preparedStatement.setInt(4, clinician.getStaff_no());
+            preparedStatement.setInt(5, appointment.getId());
+
+
+
+
+
+            preparedStatement.execute();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return 1;
     }
+    public static  void getAllAppointments(Appointment appointment, Patient patient, Clinician clinician){
+        DbConnection  dbConnection = new DbConnection();
+        try {
+            Connection con = dbConnection.connectDb();
+
+            String query = "select * from appointment";
+
+
+
+
+
+
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      System.out.print(query);
+    }
+      public static void main(String[] args){
+        Patient patient = new Patient(3,"Patient 3","paid");
+          Clinician clinician = new Clinician(4, "Hesron Mburu","Brokefingers");
+          Radiographer radiographer = new Radiographer(4,"Radio 1","radiology");
+
+        Appointment appointment = new Appointment(1,"Brian",patient,clinician);
+          appointment.setStatus("pending");
+        //  placeAppointment(appointment);
+        //  appointment.setTitle("Kelyn");
+         // updateAppointment(appointment,patient,clinician);
+          deleteAppointment(appointment);
+
+
+
+      }
 }
--- src/controller/XrayProcess.java
+++ src/controller/XrayProcess.java
@@ -2,6 +2,10 @@ package controller;
 
 import models.Appointment;
 import models.ImagingResult;
+import models.Radiographer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class XrayProcess {
 
@@ -11,8 +15,25 @@ public class XrayProcess {
      * @param imagingResult
      * @return 1 if successful, 0 otherwise
      */
-    public static int saveImagingResult(Appointment appointment, ImagingResult imagingResult){
+    public static int saveImagingResult(Appointment appointment, ImagingResult imagingResult, Radiographer radiographer){
+        DbConnection dbConnection = new DbConnection();
+        try {
+            Connection con = dbConnection.connectDb();
 
+            String query = "insert into lab_result (id, image, radiographer_staff_no, appointment_id) values(?, ?, ?, ?);";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, imagingResult.getId());
+            preparedStatement.setString(2, imagingResult.getImageUrl());
+            preparedStatement.setInt(3, radiographer.getStaff_no());
+            preparedStatement.setInt(4, appointment.getId());
+
+
+            preparedStatement.execute();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return 1;
     }
 
